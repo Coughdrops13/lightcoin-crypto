ad118e032d4a3e0429dd619d13f55c79ff3c48dd
Connor Roberts <connoobets@gmail.com>
2021-11-06 10:27:55 -0600

    used classes to create banking software

--- index.js
+++ index.js
@@ -1,29 +1,95 @@
-let balance = 500.00;
-
-class Withdrawal {
-
-  constructor(amount) {
+class Transaction {
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
-
   commit() {
-    balance -= this.amount;
+
+    if(this.isAllowed()) {
+      // console.log('allowed');
+      // console.log(this.account.balance - this.amount);
+      this.account.addTransaction(this);
+    }
+    if(!this.isAllowed()) {
+      // console.log('not allowed');
+      return false;
+    }
   }
+};
 
-}
+class Withdrawal extends Transaction {
+  get value() {
+    return -this.amount;
+  }
+  isAllowed() {
+    return this.account.balance - this.amount >= 0;
+  }
+};
 
+class Deposit extends Transaction {
+  get value() {
+    return this.amount;
+  }
+  isAllowed() {
+    return true;
+  }
+};
 
+class Account {
+  constructor(username) {
+    this.username = username;
+    this.transactions = [];
+  }
+  get balance() {
+    let balance = 0;
+    this.transactions.forEach(t => {
+      balance += t.value;
+    })
+    return balance;
+  }
+  addTransaction(transaction) {
+    this.transactions.push(transaction)
 
+  }
+};
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+const myAccount = new Account('snow-patrol');
+// const yourAccount = new Account('beegees');
+// yourAccount.balance = 2000.00;
+
+const t1 = new Deposit(50, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
-
-t2 = new Withdrawal(9.99);
+// console.log(myAccount);
+const t2 = new Deposit(50, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
+// console.log(myAccount);
+// console.log(myAccount.balance);
+const t3 = new Withdrawal(101, myAccount);
+t3.commit();
+console.log(myAccount);
+console.log(myAccount.balance);
+// const t2 = new Withdrawal(999.99, yourAccount);
+// t2.commit();
+// console.log(t2);
+// const t3 = new Deposit(999.99, myAccount);
+// t3.commit();
+// console.log(t3);
 
-console.log('Balance:', balance);
+// console.log(myAccount);
+// console.log(yourAccount);
+// // t1 = new Withdrawal(50.25, myAccount);
+// // t1.commit();
+// // console.log('Transaction 1:', t1);
+
+// // t2 = new Withdrawal(9.99, myAccount);
+// // t2.commit();
+// // console.log('Transaction 2:', t2);
+
+// // t3 = new Deposit(120.00, myAccount);
+// // t3.commit();
+// // console.log('Transaction 3:', t3);
+
+// console.log('Balance:', balance);
